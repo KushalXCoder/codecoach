a802681a18b60149694b5f11128774da01e89a77
Kushal Rathod <kushalrathod044@gmail.com>
2025-09-23 09:24:43 +0530

    Added cookie and middleware

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import checkToken from "./lib/checkToken";
+
+export const middleware = async (req: NextRequest) => {
+    const cookieStore = await cookies();
+    const cookie = cookieStore.get("token");
+
+    if(!cookie) {
+        return NextResponse.redirect(new URL("/auth/register", req.url));
+    }
+
+    const token = cookie?.value;
+    if(!token) {
+        return NextResponse.redirect(new URL("/auth/register", req.url));
+    }
+
+    const decoded = checkToken(token);
+    if(decoded === null) {
+        return NextResponse.redirect(new URL("/auth/register", req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/"],
+}
