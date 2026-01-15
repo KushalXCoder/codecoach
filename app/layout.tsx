4932af8d4ee1d3b1477fa56839695af6d1b164ee
Kushal Rathod <kushalrathod044@gmail.com>
2026-01-15 13:32:37 +0530

    add: umami script analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans, Bitcount_Grid_Double, Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import QueryProvider from "@/lib/provider/query-provider";
+import Script from "next/script";
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
@@ -33,9 +34,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${dmSans.variable} ${bitcountGridDouble.variable} ${poppins.variable} bg-black antialiased`}
-      >
+      <body className={`${dmSans.variable} ${bitcountGridDouble.variable} ${poppins.variable} bg-black antialiased`}>
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="9d02b89c-8556-4e80-ae9d-78714df83757"
+          strategy="afterInteractive"
+        />
         <QueryProvider>
           {children}
           <Toaster />
