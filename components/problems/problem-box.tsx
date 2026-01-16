6353d895d017ede6b48b789a0c355c07406425b9
Kushal Rathod <kushalrathod044@gmail.com>
2026-01-16 16:26:50 +0530

    add: clickable problem box

--- components/problems/problem-box.tsx
+++ components/problems/problem-box.tsx
@@ -12,32 +12,34 @@ const baseUrl = "https://codeforces.com/contest";
 const ProblemBox = ({ question } : ProblemBoxProps) => {
     const link = `${baseUrl}/${question.contestId}/problem/${question.index}`;
     return (
-        <div
-            key={question._id}
-            className={`flex justify-between items-center border p-3 text-primary
-            border-accent-foreground rounded-lg font-sans
-            ${question.solved ? 'bg-green-100/20' : 'hover:bg-accent-foreground'}`}
-        >
-            <div>
-                <h1 className='text-lg flex items-center gap-2'>
-                    {question.name}
-                    {question.solved && <CircleCheck className="size-4 text-blue-500 mb-0.5" />}
-                </h1>
-                <p className='text-gray-500'>Rating: {question.rating}</p>
+        <Link href={link}>
+            <div
+                key={question._id}
+                className={`flex justify-between items-center border p-3 text-primary
+                border-accent-foreground rounded-lg font-sans
+                ${question.solved ? 'bg-green-100/20' : 'hover:bg-accent-foreground'}`}
+            >
+                <div>
+                    <h1 className='text-lg flex items-center gap-2'>
+                        {question.name}
+                        {question.solved && <CircleCheck className="size-4 text-blue-500 mb-0.5" />}
+                    </h1>
+                    <p className='text-gray-500'>Rating: {question.rating}</p>
+                </div>
+                <div className="flex items-center gap-3">
+                    <Link href={link} target="_blank">
+                        <Tooltip>
+                            <TooltipTrigger>
+                                <ArrowUpRightFromSquare className="size-5" />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p className="font-sans">Visit</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </Link>
+                </div>
             </div>
-            <div className="flex items-center gap-3">
-                <Link href={link} target="_blank">
-                    <Tooltip>
-                        <TooltipTrigger>
-                            <ArrowUpRightFromSquare className="size-5" />
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            <p className="font-sans">Visit</p>
-                        </TooltipContent>
-                    </Tooltip>
-                </Link>
-            </div>
-        </div>
+        </Link>
     )
 }
 
